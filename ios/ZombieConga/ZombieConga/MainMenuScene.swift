2b1f8a30b517eb7afb696394303cf175294f3af0
ddl <d_duanlin@163.com>
2024-03-05 22:00:16 +0800

    优化主界面处理（参考实例）

--- ios/ZombieConga/ZombieConga/MainMenuScene.swift
+++ ios/ZombieConga/ZombieConga/MainMenuScene.swift
@@ -12,14 +12,10 @@ class MainMenuScene: SKScene {
     
     /// 处理触摸
     func sceneTapped(){
-        let actionBlock = SKAction.run{
-            let myScene = GameScene(size: self.size)
-            myScene.scaleMode = self.scaleMode
-            let reveal = SKTransition.doorsOpenHorizontal(withDuration: 1.5)
-            self.view?.presentScene(myScene, transition: reveal)
-        }
-        
-        self.run(actionBlock)
+        let myScene = GameScene(size:self.size)
+        myScene.scaleMode = scaleMode
+        let reveal = SKTransition.doorway(withDuration: 1.5)
+        self.view?.presentScene(myScene, transition: reveal)
     }
     
     override func didMove(to view: SKView) {
@@ -30,9 +26,6 @@ class MainMenuScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
         sceneTapped()
     }
 }
