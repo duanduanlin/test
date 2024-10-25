7192088bab244331a75108bff37255316e3effb1
ddl <d_duanlin@163.com>
2024-10-25 22:40:57 +0800

    add robot

---
+++ docker/scripts/docker_run.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+docker run -it ubuntu /bin/bash \
+#	-v /Users/duanlinding/work:/home/ubuntu/work
---
+++ robot/start_agent.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+docker run -it --rm -v /dev:/dev -v /dev/shm:/dev/shm --privileged --net=host microros/micro-ros-agent:humble udp4 --port 8090 -v4
